1b54ba3faabce53f5e3b0764c8dae3df7c74a0bb
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-09-08 11:15:29 +0200

    Banner Section

---
+++ js/script.js
@@ -0,0 +1,58 @@
+carousel();
+
+function carousel() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for(i=0 ; i<slides.length ; i++) {
+    let x =((slides[i].children[0]).children[0].getAttribute('alt').split(' '))[1];
+    if(x != 3){
+      (slides[i].children[0]).children[0].setAttribute('src',`images/waiting-0${1+parseInt(x)}.jpg`);
+      (slides[i].children[0]).children[0].setAttribute('alt',`Pic ${1+parseInt(x)}`)
+    } 
+    else{
+      (slides[i].children[0]).children[0].setAttribute('src',`images/waiting-01.jpg`);
+      (slides[i].children[0]).children[0].setAttribute('alt',`Pic 1`);
+    }
+  }
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+
+
+
+function plusDivs(n) {
+  showDivs(n);
+}
+
+function showDivs(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for(i=0 ; i < slides.length ; i++){
+    let x =((slides[i].children[0]).children[0].getAttribute('alt').split(' '))[1];
+    if(n==1){
+      if(x != 3){
+        (slides[i].children[0]).children[0].setAttribute('src',`images/waiting-0${parseInt(n)+parseInt(x)}.jpg`);
+        (slides[i].children[0]).children[0].setAttribute('alt',`Pic ${parseInt(n)+parseInt(x)}`)
+      } 
+      else{
+        (slides[i].children[0]).children[0].setAttribute('src',`images/waiting-01.jpg`);
+        (slides[i].children[0]).children[0].setAttribute('alt',`Pic 1`);
+      }
+    }
+    else{
+      {
+        if(x == 1){
+          (slides[i].children[0]).children[0].setAttribute('src',`images/waiting-03.jpg`);
+          (slides[i].children[0]).children[0].setAttribute('alt',`Pic 3`)
+        } 
+        else{
+          (slides[i].children[0]).children[0].setAttribute('src',`images/waiting-0${parseInt(n)+parseInt(x)}.jpg`);
+          (slides[i].children[0]).children[0].setAttribute('alt',`Pic ${parseInt(n)+parseInt(x)}`);
+        }
+      }
+    }
+  }
+}
+
+
